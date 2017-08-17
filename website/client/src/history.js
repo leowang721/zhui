549c90212bcba34c18ea794ae494007c9c3100f8
wangkemiao <wangkemiao@zhihu.com>
2017-08-17 10:22:53 +0800

    upgrade uis

--- website/client/src/history.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file history
- *
- * @author Leo Wang(wangkemiao@zhihu.com)
- */
-
-import createHitory from 'history/createBrowserHistory'
-
-export default createHitory()
--- website/client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @file reducers 整合
- *
- * @author Leo Wang(wangkemiao@zhihu.com)
- */
-
-export form from './form'
